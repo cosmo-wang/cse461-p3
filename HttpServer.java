a6556ab2858921d3c2ba39414e43c5fac78b9953
Cosmo Wang <yw259@cs.washington.edu>
2020-02-27 00:32:42 -0800

    ItemsReader prototype

--- HttpServer.java
+++ HttpServer.java
@@ -1,5 +1,13 @@
+import java.io.FileNotFoundException;
+
 public class HttpServer {
-  public static void main(String[] args) {
-    System.out.println("hello world");
-  }
+    public static void main(String[] args) {
+        try {
+            ItemsReader ir = new ItemsReader();
+            System.out.println("done");
+        } catch (FileNotFoundException e) {
+            System.out.println("failed to read items");
+            e.printStackTrace();
+        }
+    }   
 }
