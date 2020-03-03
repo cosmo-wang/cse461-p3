a984e09f1e7c7a2ac5505e907ee3556fd308a8af
Xinyi Wang <wxyelaine98@gmail.com>
2020-03-02 23:53:52 -0800

    quest index finished

--- indices/QuestIndex.java
+++ indices/QuestIndex.java
@@ -1,76 +1,16 @@
 package indices;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 import types.Quest;
 import utils.Utils;
 
 public class QuestIndex extends DataIndex {
-    public QuestIndex(String dirName, int baseFileCols) throws FileNotFoundException {
-        // invoke super constructor to fill some basic fields
-        super(dirName, baseFileCols);
-        // reads the base file
-        String baseFile = super.findFile(dirName, "base");
-        if (baseFile == null) {
-            throw new FileNotFoundException();
-        }
-        try (BufferedReader br = new BufferedReader(new FileReader(baseFile))) {
-            String questEntry = br.readLine();
-            while ((questEntry = br.readLine()) != null) {
-                Quest quest = new Quest(questEntry.split(Utils.COMMA_DELIMITER, baseFileCols));
-                super.idToName.put(quest.getId(), quest.getName());
-                super.nameToId.put(quest.getName(), quest.getId());
-                super.idToData.put(quest.getId(), quest);
-            }
-        } catch (IOException e) {
-          System.err.println("faield to open/read file " + baseFile);
-          e.printStackTrace();
-        }
-        // reads the monsters file
-        String monsterFile = super.findFile(dirName, "monsters");
-        if (monsterFile == null) {
-            throw new FileNotFoundException();
-        }
-        try (BufferedReader br = new BufferedReader(new FileReader(monsterFile))) {
-            String monsterEntry = br.readLine();
-            while ((monsterEntry = br.readLine()) != null) {
-                String[] info = monsterEntry.split(Utils.COMMA_DELIMITER);
-                int id = Integer.parseInt(info[0]);
-                if (this.contains(id)) {
-                    Quest quest = (Quest) super.idToData.get(id);
-                    quest.addMonster(info[1], Integer.parseInt(info[2]));
-                } else {
-                    System.err.println("error");
-                }
-            }
-        } catch (IOException e) {
-          System.err.println("faield to open/read file " + monsterFile);
-          e.printStackTrace();
-        }
-        // reads the rewards file
-        String rewardFile = super.findFile(dirName, "rewards");
-        if (rewardFile == null) {
-            throw new FileNotFoundException();
-        }
-        try (BufferedReader br = new BufferedReader(new FileReader(rewardFile))) {
-            String rewardEntry = br.readLine();
-            while ((rewardEntry = br.readLine()) != null) {
-                String[] info = rewardEntry.split(Utils.COMMA_DELIMITER);
-                int id = Integer.parseInt(info[0]);
-                if (this.contains(id)) {
-                    Quest quest = (Quest) super.idToData.get(id);
-                    quest.addRewards(info[2], Integer.parseInt(info[3]), Integer.parseInt(info[3]));
-                } else {
-                    System.err.println("error");
-                }
-            }
-        } catch (IOException e) {
-          System.err.println("faield to open/read file " + rewardFile);
-          e.printStackTrace();
-        }
+    public QuestIndex(String dirName) throws FileNotFoundException {
+        super(dirName);
+        super.readFile("base", this::addBase);
+        super.readFile("monster", this::addMonster);
+        super.readFile("rewards", this::addRewards);
     }
 
     public boolean contains(String questName) {
@@ -94,6 +34,29 @@ public class QuestIndex extends DataIndex {
             System.err.println("Item could not be cloned.");
             e.printStackTrace();
             return null;
+        }
+    }
+
+    private void addBase(String[] info) {
+        Quest quest = new Quest(info);
+        super.idToName.put(quest.getId(), quest.getName());
+        super.nameToId.put(quest.getName(), quest.getId());
+        super.idToData.put(quest.getId(), quest);
+    }
+
+    private void addMonster(String[] info) {
+        int id = Utils.parseInt(info[0]);
+        if (this.contains(id)) {
+            Quest quest = (Quest) super.idToData.get(super.nameToId.get(info[0]));
+            quest.addMonster(info[1], Utils.parseInt(info[2]));
+        }
+    }
+
+    private void addRewards(String[] info) {
+        int id = Utils.parseInt(info[0]);
+        if (this.contains(id)) {
+            Quest quest = (Quest) super.idToData.get(super.nameToId.get(info[0]));
+            quest.addRewards(info[2], Utils.parseInt(info[3]), Utils.parseInt(info[4]));
         }
     }
 }
